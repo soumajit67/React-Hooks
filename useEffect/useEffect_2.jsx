7500f987a85bbb2e7c0bf88ee51746089079aef9
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-06-29 09:21:34 +0530

    Update useEffect_2.jsx

--- useEffect/useEffect_2.jsx
+++ useEffect/useEffect_2.jsx
@@ -1,1 +1,24 @@
 
+import { useState, useEffect } from "react";
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
